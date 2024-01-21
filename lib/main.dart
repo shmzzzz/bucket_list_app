86abdea76aeb951aa2e69122824636dbab44ed68
shmzzzz <slt0623@gmail.com>
2024-01-21 11:24:56 +0900

    ✨ カスタムフォントをアプリ全体に適用する

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
         colorScheme: MaterialTheme.lightScheme().toColorScheme(),
+        textTheme: MaterialTheme.openSansTextTheme(context),
       ),
       darkTheme: ThemeData(
         colorScheme: MaterialTheme.darkScheme().toColorScheme(),
+        textTheme: MaterialTheme.openSansTextTheme(context),
       ),
       home: const LoginPage(),
     );
