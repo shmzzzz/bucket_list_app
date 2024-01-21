1c8d9244d5ce09f27039069704efe710656cba34
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-01-21 17:37:17 +0900

    move: main.dartをpresentation/page配下に移動する (#22)
    * 🚚 main.dartをpresentation/page配下に移動する
    * 🚚 main.dartのパスを変更する

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bucket_list_app/main.dart';
+import 'package:bucket_list_app/presentation/page/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
