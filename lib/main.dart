0bd737798eebe89e16b8686cbc17edc72895b288
shmz <85086833+shmzzzz@users.noreply.github.com>
2024-01-28 21:25:01 +0900

    feat: Riverpodを導入する #21 (#28)
    * 📦 Riverpodの導入
    * ✨ ProvierScopeを設定する
    * 📦 hooks_riverpodの導入

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:bucket_list_app/infrastructure/firebase_options.dart';
 import 'package:bucket_list_app/presentation/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
