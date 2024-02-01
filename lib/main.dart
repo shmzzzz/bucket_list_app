968926028b3b5761ee7e8cfa137c6cd612c1fb45
shmzzzz <slt0623@gmail.com>
2024-02-01 20:20:08 +0900

    🛂 ログイン機能の追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:bucket_list_app/application/state/auth_state_changes_provider.dart';
 import 'package:bucket_list_app/common/router.dart';
 import 'package:bucket_list_app/infrastructure/firebase_options.dart';
+import 'package:bucket_list_app/presentation/page/list_page.dart';
+import 'package:bucket_list_app/presentation/page/login_page.dart';
 import 'package:bucket_list_app/presentation/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +33,20 @@ class MyApp extends StatelessWidget {
         colorScheme: MaterialTheme.darkScheme().toColorScheme(),
         textTheme: MaterialTheme.openSansTextTheme(context),
       ),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authStateAsync = ref.watch(authStateChangesProvider);
+    return authStateAsync.when(
+      data: (user) => user != null ? const ListPage() : const LoginPage(),
+      loading: () => const CircularProgressIndicator(),
+      error: (err, stack) => Text('Error: $err'),
     );
   }
 }
--- lib/presentation/page/login_page.dart
+++ lib/presentation/page/login_page.dart
@@ -1,11 +1,77 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends HookWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final firebase = FirebaseAuth.instance;
+
+    final emailFormKey = GlobalKey<FormState>();
+    final passwordFormKey = GlobalKey<FormState>();
+
+    final emailController = useTextEditingController(text: '');
+    final passwordController = useTextEditingController(text: '');
+
+    final isObscured = useState(false);
+    final isLogin = useState(false);
+    final isAuthenticating = useState(false);
+
+    void submit() async {
+      final isValid = emailFormKey.currentState!.validate() &&
+          passwordFormKey.currentState!.validate();
+
+      // currentStateがvalidateでない場合は処理を行わない
+      if (!isValid) {
+        return;
+      }
+
+      emailFormKey.currentState!.save();
+      passwordFormKey.currentState!.save();
+
+      try {
+        isAuthenticating.value = true;
+        if (isLogin.value) {
+          // ログイン時の処理
+          (await firebase.signInWithEmailAndPassword(
+            email: emailController.value.text,
+            password: passwordController.value.text,
+          ))
+              .user;
+          context.go('/detail');
+        } else {
+          // 新規登録時の処理
+          final User? user = (await firebase.createUserWithEmailAndPassword(
+            email: emailController.value.text,
+            password: passwordController.value.text,
+          ))
+              .user;
+          context.go('/mypage');
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user!.uid)
+              .set({
+            'username': 'ゲスト',
+            'email': emailController.value.text,
+          });
+          isLogin.value = true;
+        }
+      } on FirebaseAuthException catch (error) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(error.message ?? 'Authentication failed'),
+          ),
+        );
+        // エラーが発生しているので認証処理は行っていない
+        isAuthenticating.value = false;
+      }
+    }
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 45),
@@ -21,37 +87,72 @@ class LoginPage extends StatelessWidget {
             ),
             const SizedBox(height: 80),
             Form(
+              key: emailFormKey,
               child: TextFormField(
+                controller: emailController,
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Email',
                 ),
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                textCapitalization: TextCapitalization.none,
+                validator: (value) {
+                  if (value == null ||
+                      value.trim().isEmpty ||
+                      !value.contains('@')) {
+                    return 'メールアドレスを入力してください。';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  emailController.text = newValue!;
+                },
               ),
             ),
             const SizedBox(height: 20),
             Form(
+              key: passwordFormKey,
               child: TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
+                controller: passwordController,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
                   labelText: 'Password',
-                  suffixIcon: Icon(Icons.visibility_off_outlined),
+                  suffixIcon: IconButton(
+                    icon: isObscured.value
+                        ? const Icon(Icons.visibility)
+                        : const Icon(Icons.visibility_off_outlined),
+                    onPressed: () {
+                      isObscured.value = !isObscured.value;
+                    },
+                  ),
                 ),
+                obscureText: isObscured.value,
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'パスワードを入力してください。';
+                  }
+                  return null;
+                },
+                onSaved: (newValue) {
+                  passwordController.text = newValue!;
+                },
               ),
             ),
             const SizedBox(height: 40),
             OutlinedButton(
-              onPressed: () {
-                context.go('/list');
-              },
-              child: const Text(
-                'LOGIN',
+              onPressed: submit,
+              child: Text(
+                isLogin.value ? 'LOGIN' : 'CREATE ACCOUNT',
               ),
             ),
             const SizedBox(height: 20),
-            OutlinedButton(
-              onPressed: () {},
-              child: const Text(
-                'CREATE ACCOUNT',
+            TextButton(
+              onPressed: () {
+                isLogin.value = !isLogin.value;
+              },
+              child: Text(
+                isLogin.value ? 'CREATE ACCOUNT' : 'LOGIN',
               ),
             ),
           ],
