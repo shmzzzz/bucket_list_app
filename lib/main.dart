fb8ec86d11c111c853508c9c6956133cc76cde2a
shmzzzz <slt0623@gmail.com>
2024-02-02 10:19:12 +0900

    🎨 パス変更に伴う修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:bucket_list_app/application/state/auth_state_changes_provider.dart';
-import 'package:bucket_list_app/common/router.dart';
+import 'package:bucket_list_app/presentation/router/router.dart';
 import 'package:bucket_list_app/infrastructure/firebase_options.dart';
 import 'package:bucket_list_app/presentation/page/list_page.dart';
 import 'package:bucket_list_app/presentation/page/login_page.dart';
-import 'package:bucket_list_app/presentation/theme.dart';
+import 'package:bucket_list_app/presentation/theme/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
