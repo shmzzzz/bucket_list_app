529e88c7e0e1538d49a99dfaa82470cadec88197
shmzzzz <slt0623@gmail.com>
2024-06-29 14:20:37 +0900

    🎨 入力した「やりたい度」を登録できるようにする

--- lib/presentation/widgets/list_item/item_add_button.dart
+++ lib/presentation/widgets/list_item/item_add_button.dart
@@ -1,3 +1,4 @@
+import 'package:bucket_list_app/application/state/wish_level_notifier.dart';
 import 'package:bucket_list_app/presentation/router/router.dart';
 import 'package:bucket_list_app/presentation/theme/app_strings.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,22 +16,23 @@ class ItemAddButton extends ConsumerStatefulWidget {
 
 class _ItemAddButtonState extends ConsumerState<ItemAddButton> {
   String inputTitle = '';
-  String inputWishLevel = '';
   DateTime inputDue = DateTime.now();
   String inputCategory = '';
   String inputMemo = '';
 
   @override
   Widget build(BuildContext context) {
+    final inputWishLevel = ref.watch(wishLevelNotifierProvider);
+
     return ListTile(
       title: const Text(
         AppStrings.addButton,
       ),
-      onTap: _submitData,
+      onTap: () => _submitData(inputWishLevel),
     );
   }
 
-  void _submitData() {
+  void _submitData(int wishLevel) {
     try {
       // ユーザーのUIDを取得
       String userUid = FirebaseAuth.instance.currentUser!.uid;
@@ -41,7 +43,7 @@ class _ItemAddButtonState extends ConsumerState<ItemAddButton> {
       FirebaseFirestore.instance.collection(userPath).add(
         {
           'title': inputTitle,
-          'wish_level': inputWishLevel,
+          'wish_level': wishLevel,
           'due': inputDue,
           'memo': inputMemo,
           'category': inputCategory,
