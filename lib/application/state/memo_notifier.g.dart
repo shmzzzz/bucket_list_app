bd269d39172f358b1d9fccbf00f3b1edeea977b8
shmzzzz <slt0623@gmail.com>
2024-06-29 16:52:30 +0900

    👔 メモのRiverpod関連のファイルを追加

---
+++ lib/application/state/memo_notifier.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'memo_notifier.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$memoNotifierHash() => r'9b0a85199516c00e2a7fcde454dd72c1d1ffb079';
+
+/// See also [MemoNotifier].
+@ProviderFor(MemoNotifier)
+final memoNotifierProvider =
+    AutoDisposeNotifierProvider<MemoNotifier, String>.internal(
+  MemoNotifier.new,
+  name: r'memoNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$memoNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MemoNotifier = AutoDisposeNotifier<String>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
