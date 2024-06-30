08cb8ab2ec311f32a19ba5f992c6b11f655d0a48
shmzzzz <slt0623@gmail.com>
2024-06-30 14:53:39 +0900

    🎨 達成したかをRiverpodで管理する

--- lib/presentation/widgets/list_item/input_checkbox.dart
+++ lib/presentation/widgets/list_item/input_checkbox.dart
@@ -1,22 +1,24 @@
+import 'package:bucket_list_app/application/state/achieved_notifier.dart';
 import 'package:bucket_list_app/presentation/theme/sizes.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class InputCheckbox extends HookWidget {
+class InputCheckbox extends ConsumerWidget {
   const InputCheckbox({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final isChecked = useState(false);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isCheckedNotifier = ref.watch(achievedNotifierProvider.notifier);
+    final isChecked = ref.watch(achievedNotifierProvider);
 
     return SizedBox(
       height: Sizes.s24,
       width: Sizes.s24,
       child: Checkbox(
-        value: isChecked.value,
+        value: isChecked,
         onChanged: (value) {
           if (value != null) {
-            isChecked.value = value;
+            isCheckedNotifier.toggleState();
           }
         },
       ),
